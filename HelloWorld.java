365a228e31d3a14465b080f212677b78505cbfd1
JHeist <42556389+JHeist@users.noreply.github.com>
2018-08-20 17:52:18 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,21 @@
-// John Doe
+// Jeremy Heist
 
-public class Main
+public class Main 
 {
-
-   public static void main(String[] args)
-   {
-      System.out.println("hello world");
-   }
-
+  
+  public static void main(String[] args) 
+  {
+    System.out.println("     Penn State Alma Mater: ");
+    System.out.println();
+    System.out.println("   For the Glory of Old State");
+    System.out.println("For her founders strong and great.");
+    System.out.println("   For the future that we wait,");
+    System.out.println("  Raise the song, raise the song. ");
+    System.out.println();
+    System.out.println("    Sing our love and loyalty,");
+    System.out.println("Sing our hopes that bright and free");
+    System.out.println("  Rest, O Mother, dear with thee ");
+    System.out.println("  All with thee, all with thee. ");
+  }
+  
 }
